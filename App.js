097618589d909384eee6aa6bf514f1d04de57669
Guilherme Eiras <eduardo.lopes@codus.com.br>
2019-09-13 10:37:52 -0300

    feat(MainScreen): Fix get raw value method and refactor calculation method

--- App.js
+++ App.js
@@ -13,11 +13,21 @@ class App extends Component {
     super(props);
     this.state = {
       value: '',
-      tamanho: '',
+      size: '',
     };
   }
-
+  calculation(value, size, quantity) {
+    const convertedValue = parseFloat(value.replace(/[^0-9.-]+/g, ''));
+    return (((convertedValue / size) * quantity) / 100).toFixed(2);
+  }
   render() {
+    const volumes = [
+      {recipient: 'litrao 1000ml', quantity: 1000},
+      {recipient: 'garrafa 600ml', quantity: 600},
+      {recipient: 'lata 350ml', quantity: 350},
+      {recipient: 'latinha 269ml', quantity: 269},
+    ];
+
     return (
       <View style={styles.page}>
         <View style={styles.header}>
@@ -36,25 +46,31 @@ class App extends Component {
             ref={ref => (this.moneyField = ref)}
           />
           <Picker
-            selectedValue={this.state.tamanho}
+            selectedValue={this.state.size}
             onValueChange={(itemValue, itemIndex) =>
-              this.setState({tamanho: itemValue})
+              this.setState({size: itemValue})
             }
             style={styles.pickerStyle}>
             <Picker.Item label="Piriguete" value={269} />
             <Picker.Item label="Normal" value={350} />
           </Picker>
           <Divider />
-          <TextInput
-            mode="outlined"
-            label="Litrão"
-            value={
-              ('R$' + ((this.moneyField.getRawValue() / this.state.tamanho) * 1000))
-            }
-            disabled
-          />
-          <Text>{this.moneyField.getRawValue()}</Text>
-          <Text>{this.state.tamanho}</Text>
+          {volumes.map((volume, index) => (
+            <TextInput
+              key={index}
+              mode="outlined"
+              label={volume.recipient}
+              value={
+                'R$' +
+                this.calculation(
+                  this.state.value,
+                  this.state.size,
+                  volume.quantity,
+                )
+              }
+              disabled
+            />
+          ))}
         </View>
       </View>
     );
