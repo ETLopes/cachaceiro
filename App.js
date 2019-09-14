b8709f38e7bd1fc845849e8bea0a7e09d5ea858c
Guilherme Eiras <eduardo.lopes@codus.com.br>
2019-09-13 21:04:20 -0300

    feat(MainScreen): style the main page

--- App.js
+++ App.js
@@ -4,8 +4,8 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, Picker} from 'react-native';
-import {TextInput, Divider} from 'react-native-paper';
+import {StyleSheet, View, Text, Picker, ScrollView} from 'react-native';
+import {TextInput, Divider, Appbar} from 'react-native-paper';
 import {TextInputMask} from 'react-native-masked-text';
 
 class App extends Component {
@@ -13,7 +13,7 @@ class App extends Component {
     super(props);
     this.state = {
       value: '',
-      size: '',
+      size: 1000,
     };
   }
   calculation(value, size, quantity) {
@@ -22,18 +22,26 @@ class App extends Component {
   }
   render() {
     const volumes = [
-      {recipient: 'litrao 1000ml', quantity: 1000},
-      {recipient: 'garrafa 600ml', quantity: 600},
-      {recipient: 'lata 350ml', quantity: 350},
-      {recipient: 'latinha 269ml', quantity: 269},
+      {recipient: 'Litrao 1000ml', quantity: 1000},
+      {recipient: 'Garrafa 600ml', quantity: 600},
+      {recipient: 'Garrafa Bud 550ml', quantity: 550},
+      {recipient: 'Latão 473ml', quantity: 473},
+      {recipient: 'Latão Colorado 410ml', quantity: 410},
+      {recipient: 'Long Neck 355ml', quantity: 355},
+      {recipient: 'Lata 350ml', quantity: 350},
+      {recipient: 'Lata Stella 310ml', quantity: 310},
+      {recipient: 'Litrinho 300ml', quantity: 300},
+      {recipient: 'Long Neck Stella 275ml', quantity: 275},
+      {recipient: 'Latinha 269ml', quantity: 269},
     ];
 
     return (
       <View style={styles.page}>
-        <View style={styles.header}>
-          <Text>Conversor de preço para cervejas</Text>
-        </View>
+        <Appbar.Header style={styles.header}>
+          <Appbar.Content title="Conversor de Cervejas" />
+        </Appbar.Header>
         <View style={styles.body}>
+          <Text>Digite o preço da cerveja:</Text>
           <TextInputMask
             label={'preço'}
             type={'money'}
@@ -44,33 +52,46 @@ class App extends Component {
               });
             }}
             ref={ref => (this.moneyField = ref)}
+            style={styles.valueInput}
           />
+          <Text>Escolha o tamanho do vasilhame:</Text>
           <Picker
             selectedValue={this.state.size}
             onValueChange={(itemValue, itemIndex) =>
               this.setState({size: itemValue})
             }
             style={styles.pickerStyle}>
-            <Picker.Item label="Piriguete" value={269} />
-            <Picker.Item label="Normal" value={350} />
+            {volumes.map((volume, index) => (
+              <Picker.Item
+                key={index}
+                label={volume.recipient}
+                value={volume.quantity}
+              />
+            ))}
           </Picker>
           <Divider />
-          {volumes.map((volume, index) => (
-            <TextInput
-              key={index}
-              mode="outlined"
-              label={volume.recipient}
-              value={
-                'R$' +
-                this.calculation(
-                  this.state.value,
-                  this.state.size,
-                  volume.quantity,
-                )
-              }
-              disabled
-            />
-          ))}
+          <View style={styles.valuesArea}>
+            {volumes.map((volume, index) => (
+              <View style={styles.valueDisplayBox} k
+              ey={index}>
+                <TextInput
+                  key={index}
+                  style={styles.valueDisplay}
+                  mode="outlined"
+                  label={volume.recipient}
+                  value={
+                    'R$' +
+                    this.calculation(
+                      this.state.value,
+                      this.state.size,
+                      volume.quantity,
+                    )
+                  }
+                  disabled
+                />
+              </View>
+            ))}
+          </View>
         </View>
       </View>
     );
@@ -82,15 +103,36 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   header: {
-    flex: 2,
+    elevation: 4,
   },
   body: {
     flex: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  valueInput: {
+    borderRadius: 5,
+    borderWidth: 1,
+    paddingLeft: 10,
   },
   pickerStyle: {
+    borderRadius: 5,
     borderWidth: 1,
     backgroundColor: '#EEEEEE',
-    borderRadius: 20,
+  },
+  valuesArea: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  valueDisplayBox: {
+    width: '48%',
+    marginLeft: 5,
+  },
+  valueDisplay: {
+    marginTop: 15,
+    color: 'black'
   },
 });
 
