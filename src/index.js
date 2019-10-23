5fa5b4e7b3233914d62ce2b808a6b58bb511c90f
Eduardo <eduardotorreslopes@gmail.com>
2019-10-23 00:36:41 -0300

    feat(QRCode): add files to be opened with qr code lib

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import {createStackNavigator} from 'react-navigation-stack';
 
 import Home from './Home';
 import About from './About';
+import QRCode from './QRCode';
 
 const Routes = createAppContainer(
   createStackNavigator({
+    QRCode: QRCode,
     Home: Home,
     About: About,
   }),
